c7b4431cc535636cc37fd7562fc600698e2d1db1
Elizabeth Stilwell <elizabethstilwell@gmail.com>
2012-11-24 23:02:21 -0500

    nicer server message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var express = require('express')
   , app = express()
-  , port = 9000;
+  , port = 9999;
 
 
 app.configure(function () {
@@ -11,4 +11,15 @@ app.configure(function () {
 });
 
 app.listen(port);
-console.log("Server Running on " + port);
+console.log("\n\n\n");
+console.log("                Hello Elizabeth.");
+console.log();
+console.log("You website is running at http://localhost:" + port);
+console.log("When you are finished, come back to this window and");
+console.log("press [control + c] to stop the server");
+console.log();
+console.log("And have a nice day.");
+console.log();
+console.log("❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤");
+console.log();
+console.log("Nathan");
